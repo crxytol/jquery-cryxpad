d9d075cb41282084e1b0fe79bf3ba0a7cafd9cba
Ange Michael Atse <michael.atse@ci.12thwonder.com>
2018-05-28 14:31:28 +0000

    seconde commit

--- jquery.cryxpad.js
+++ jquery.cryxpad.js
@@ -1,9 +1,9 @@
 /*
 *
 * Nom du pluging : cryxpad
-* Auteur: Atsé yapi ange michael
+* Auteur: Atsé Y. ange michael
 * Version 1.0
-* Requirement : jQuery v1.11.3,Bootstrap v3.3.6 (http://getbootstrap.com)
+* Requirement : jQuery v1.11.3,Bootstrap v4 (http://getbootstrap.com)
 */
 
 (function($)
@@ -19,7 +19,7 @@
             'removeButtonId':'cryxpad-remove-btn',
             'validateButtonId':'cryxpad-validate-btn',
             'containerpadding':10,
-    		'carreaux':10,
+            'carreaux':10,
     		'width': 50,		//Largeur de la galerie
     		'height': 50,		//Hauteur de la galerie
             'button':{
@@ -28,136 +28,122 @@
                 'marginTop':3,
                 'marginBottom':3,
             },
-    		'callback': null
+            'callback': null
 		      //Fonction appelée à chaque nouvelle image
-		};  
-        
-		//var positionGenerees =[];
+        };
+        //var positionGenerees =[];
 
-            //On fusionne nos deux objets ! =D
+        //On fusionne nos deux objets ! =D
         var parametres=$.extend(defauts, options);
         var $inputcodeElement = $('input#'+parametres.inputFormId+'');
         $inputcodeElement.prop('readonly', true);
 
         var $removeBtnElement = $('input#'+parametres.removeButtonId+'');
-        
-        
+
+
         var selectednumber=null;
 
         var fieldArray = [];
         return this.each(function(){
-        	
-        	var element=$(this);
-        	var container = $('.'+parametres.containerClass+'');
-        	/*var inputtextfiled =$('input#input-clavier');
-        	inputtextfiled.prop('readonly', true);
-            */
-            
-        	element.css(
-			{
+
+           var element=$(this);
+           var container = $('.'+parametres.containerClass+'');
+    	
+        
+            element.css(
+            {
                 'position': 'relative',
                 'margin-left': 'auto',
                 'margin-right': 'auto',
-				'width': (parametres.width *parametres.carreaux)+(parametres.carreaux *parametres.button.marginLeft)+3,
-				'height': (parametres.width *parametres.carreaux)+(parametres.carreaux *parametres.button.marginLeft)+3,
-			    'background-color': 'rgb(241, 241, 241)',
+                'width': (parametres.width *parametres.carreaux)+(parametres.carreaux *parametres.button.marginLeft)+3,
+                'height': (parametres.width *parametres.carreaux)+(parametres.carreaux *parametres.button.marginLeft)+3,
+                'background-color': 'rgb(241, 241, 241)',
                 'left': '-50%'
-			});
-			
-        	var numberarray=[0,1,2,3,4,5,6,7,8,9];
-        		for (i = 0; i < parametres.carreaux; i++) 
-				{
-					
-        			var a = new Array(parametres.carreaux);
-					a[i] = new Array(parametres.carreaux);
-					for (j = 0; j < parametres.carreaux; j++) 
-					{
-					a[i][j] = "[" + i + "," + j + "]";
-						
-					
-					var tt = $("<button type='button' class='code-btn'></button>").css({
-						'position': 'absolute',
-						'width': parametres.width,
-						'height': parametres.height,
-						'text-align': "center",
-						'top': (j*parametres.height),
-    					'left': i*parametres.width,
+            });
+
+            var numberarray=[0,1,2,3,4,5,6,7,8,9];
+            for (i = 0; i < parametres.carreaux; i++) {
+                var a = new Array(parametres.carreaux);
+                a[i] = new Array(parametres.carreaux);
+                for (j = 0; j < parametres.carreaux; j++) {
+                    a[i][j] = "[" + i + "," + j + "]";
+                    var tt = $("<button type='button' class='code-btn'></button>").css({
+                        'position': 'absolute',
+                        'width': parametres.width,
+                        'height': parametres.height,
+                        'text-align': "center",
+                        'top': (j*parametres.height),
+                        'left': i*parametres.width,
                         'margin-left':(i*parametres.button.marginLeft)+3,
                         'margin-top':(j*parametres.button.marginTop)+3
-    				}).addClass(''+parametres.button.class+'');
-					tt.attr({
-						'id':j+"_"+i+"_div"
-					});
-                    //tt.attr("style",parametres.button.style);
-					tt.appendTo(element);
-					}
-					
-				}
-				
-    
-    			for (var i=0; i<10; i++) { 
-    				var rep =false
-        			var tt = fieldArray.length;
-        			if(tt ==0)
-        			{
-        				var x = getRandomInt(0,parametres.carreaux-1); 
-        				var y = getRandomInt(0,parametres.carreaux-1); 
-        				var data={
-        					x : x,
-        					y: y
-        				}
-        				fieldArray[i]=data;
-        			}
-        			else if(fieldArray.length>0)
-        			{        				
-        				var data={};
-						data.x = getRandomInt(0,parametres.carreaux-1); 
-        				data.y = getRandomInt(0,parametres.carreaux-1); 
-        					
-        				rep = ifExist(fieldArray,data);
-     					while(!rep){
-     						data.x = getRandomInt(0,parametres.carreaux-1); 
-        					data.y = getRandomInt(0,parametres.carreaux-1); 
-     						rep = ifExist(fieldArray,data);
-     						}
-     						fieldArray[i]=data;
-        			}
-        		}
-				var i=0;
-        		fieldArray.forEach(function(p){
-        			var elId = p.y+"_"+p.x+"_div";
-        			var el = $('button[id="'+elId+'"]');
-                    el.attr("data-number",i);
-                    //el.html('<span style="color:with;">'+i+'</span>');
-        			el.html(i);
-        			i++;
-        		});
+                    }).addClass(''+parametres.button.class+'');
+                    tt.attr({
+                        'id':j+"_"+i+"_div"
+                    });
+                    tt.appendTo(element);
+                }
+            }
 
-                $(document).on("click",'.'+parametres.containerClass+' .code-btn',function(e) {
-                    debugger;
-                    e.preventDefault();
-                    e.stopPropagation();
-                    buttonClik($(this));
-                });
-                /*
-                * remove bnt click
-                */
-                $(document).on("click",'#'+parametres.removeButtonId+'',function(e) {
-                    debugger;
-                    e.preventDefault();
-                    e.stopPropagation();
-                    removeButtonClick($(this))
-                });
+            for (var i=0; i<10; i++) { 
+                var rep =false
+                var tt = fieldArray.length;
+                if(tt ==0){
+                    var x = getRandomInt(0,parametres.carreaux-1); 
+                    var y = getRandomInt(0,parametres.carreaux-1); 
+                    var data={
+                       x : x,
+                       y: y
+                   }
+                   fieldArray[i]=data;
+               }
+               else if(fieldArray.length>0){
+                    var data={};
+                    data.x = getRandomInt(0,parametres.carreaux-1); 
+                    data.y = getRandomInt(0,parametres.carreaux-1); 
 
-                $(document).on("click",'#'+parametres.validateButtonId+'',function(e) {
-                    debugger;
-                    e.preventDefault();
-                    e.stopPropagation();
-                    validaterButtonClick($(this))
-                });
-                
-	    });
-        
+                    rep = ifExist(fieldArray,data);
+                    while(!rep){
+                        data.x = getRandomInt(0,parametres.carreaux-1); 
+                        data.y = getRandomInt(0,parametres.carreaux-1); 
+                        rep = ifExist(fieldArray,data);
+                    }
+                    fieldArray[i]=data;
+                }
+            }
+            var i=0;
+            fieldArray.forEach(function(p){
+                var elId = p.y+"_"+p.x+"_div";
+                var el = $('button[id="'+elId+'"]');
+                el.attr("data-number",i);
+                //el.html('<span style="color:with;">'+i+'</span>');
+                el.html(i);
+                i++;
+            });
+
+            $(document).on("click",'.'+parametres.containerClass+' .code-btn',function(e) {
+                debugger;
+                e.preventDefault();
+                e.stopPropagation();
+                buttonClik($(this));
+            });
+            /*
+            * remove bnt click
+            */
+            $(document).on("click",'#'+parametres.removeButtonId+'',function(e) {
+                debugger;
+                e.preventDefault();
+                e.stopPropagation();
+                removeButtonClick($(this))
+            });
+
+            $(document).on("click",'#'+parametres.validateButtonId+'',function(e) {
+                debugger;
+                e.preventDefault();
+                e.stopPropagation();
+                validaterButtonClick($(this))
+            });  
+        });
+
         function buttonClik($el){
             debugger;
             if($el.data("number") !== undefined){
@@ -182,32 +168,30 @@
         };
 
     };
-
     /*
-* fonctions utile
-**
-*/
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function ifExist(dd,data){
-
-	for (var i = 0; i < dd.length; i++)
-    {
-        if(dd[i].x ==data.x && dd[i].y ==data.y)
-		{
-			console.log("p.x="+dd[i].x+" / data.x="+data.x);
-			console.log("p.y="+dd[i].y+" / data.y="+data.y);
-			return false;
-		}
+    * usefull functions
+    **
+    */
+    function getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
     }
-    return true;
-}
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function ifExist(dd,data){
+
+    	for (var i = 0; i < dd.length; i++)
+        {
+            if(dd[i].x ==data.x && dd[i].y ==data.y){
+                console.log("p.x="+dd[i].x+" / data.x="+data.x);
+                console.log("p.y="+dd[i].y+" / data.y="+data.y);
+                return false;
+            }
+        }
+        return true;
+    }
 
 })(jQuery);
 
